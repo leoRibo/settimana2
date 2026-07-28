d899a9eb60e2e07ebe2e652b575881ab4518df09
leoRibo <leonriebel2001@gmail.com>
2026-07-28 16:53:09 +0200

    esercizi for

---
+++ for.py
@@ -0,0 +1,23 @@
+spesa = ["pane", "latte", "uova", "caffe"]
+
+print("Lista della spesa:")
+for prodotto in spesa:
+    print(f"- {prodotto}")
+
+print(f"In totale hai {len(spesa)} prodotti")
+
+numeri = [10, 20, 30, 40]
+somma = 0 
+for n in numeri:
+    somma = somma + n
+print(f"La somma è {somma}")
+
+for i in range(5):
+    print(f"giro numero {i}")
+
+film = ["Matrix", "Inception", "Interstellar"]
+print("voglio giurdare questi film:")
+
+for uscita in film:
+    print(f"- {uscita}")
+    
