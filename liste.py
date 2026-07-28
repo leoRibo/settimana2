fba9553e22eb259f5a1aeb0b58576202e9990c74
leoRibo <leonriebel2001@gmail.com>
2026-07-28 16:17:30 +0200

    esercizio sulle liste

---
+++ liste.py
@@ -0,0 +1,11 @@
+film = ["Matrix", "Inception", "Interstellar"]
+roba = []
+
+print(f"Ho {len(film)} film preferiti")
+print(f"il primo film è {film[0]}.")
+
+film.append("odissea")
+film.append("peppa pig")
+print(f"ne ho agigunti  due, ora sono {len(film)}.")
+print(f"l'ultimo film che ho aggiuto alla lista è {film[len(film)-1]}.")
+print(len(roba))
