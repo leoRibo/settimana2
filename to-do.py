1a91687871cc8df2dd0bbc196ed48a96a73347e6
leoRibo <leonriebel2001@gmail.com>
2026-07-28 20:35:43 +0200

    programma finito ma migliorabile e con bugs

--- to-do.py
+++ to-do.py
@@ -8,12 +8,22 @@ for numero, istruzione in enumerate(attivita):
     print(f"{numero+1}. {stato}   {istruzione['compito']}")
 
 
-utente = input("metti un nuovo compito")
-attivita.append({"compito": utente, "fatto": False})
+utente = input("C'é qualcosa di nuovo?")
+if utente.lower() != "no":
+    attivita.append({"compito": utente, "fatto": False})
 
 for numero, istruzione in enumerate(attivita):
     stato = "✅" if istruzione["fatto"] else "❌"
     print(f"{numero+1}. {stato}   {istruzione['compito']}")
 
+cambio = int(input("che cosa cambi?"))
+if attivita[cambio-1]["fatto"] == False: 
+    attivita[cambio-1]["fatto"] = True 
+else:
+    attivita[cambio-1]["fatto"] = False
+
+for numero, istruzione in enumerate(attivita):
+    stato = "✅" if istruzione["fatto"] else "❌"
+    print(f"{numero+1}. {stato}   {istruzione['compito']}")
 
 
