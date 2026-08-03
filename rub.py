f087bbbe316851d490fb4003bdd0fe1ec426143a
leoRibo <leonriebel2001@gmail.com>
2026-08-03 14:01:36 +0200

    tappa 2 rubrica

--- rub.py
+++ rub.py
@@ -5,8 +5,21 @@ rubbrica = [
 
 def mostra_contatti(lista):
     for numero, contatto in enumerate(lista):
-        print(f"{numero+1}. {contatto['nome']} - {contatto['numero_cell'][0:3]} - {contatto['numero_cell'][3:]}")
+        print(f"{numero+1}. {contatto['nome']} - {contatto['numero_cell'][0:3]}-{contatto['numero_cell'][3:]}")
 
 mostra_contatti(rubbrica)
+
+def aggiungi_contatto(lista):
+    nuovo_nome = input("aggiugi nome").title()
+    nuovo_numero = input("aggiugi il numero di telefono").title()
+    lista.append({"nome": nuovo_nome, "numero_cell": nuovo_numero})
+
+aggiungi_contatto(rubbrica)
+mostra_contatti(rubbrica)
+
+
+
+
+
 
         
